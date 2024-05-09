4c331862b7e607a1713f550d2dd29d7594f44351
Carlossanlo <carlossanlo@unisabana.edu.co>
2024-05-09 12:12:07 -0500

    parte 3

--- Front/login.js
+++ Front/login.js
@@ -1,43 +1,47 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evitar que el formulario se envíe de forma predeterminada
-  
-    // Obtener los valores de usuario y contraseña del formulario
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-  
-    // Objeto con los datos del formulario a enviar
-    const formData = {
-      username: username,
-      password: password
-    };
-  
-    // Configurar la solicitud POST
-    const requestOptions = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(formData)
-    };
-  
-    // Realizar la solicitud POST utilizando Fetch API
-    fetch('http://tu-endpoint.com/login', requestOptions)
-      .then(response => {
-        // Verificar si la respuesta es exitosa
-        if (!response.ok) {
-          throw new Error('Error al iniciar sesión: ' + response.statusText);
-        }
-        // Convertir la respuesta a formato JSON
-        return response.json();
-      })
-      .then(data => {
-        // Manejar la respuesta JSON
-        console.log('Respuesta del servidor:', data);
-        // Aquí podrías redirigir a otra página, mostrar un mensaje de éxito, etc.
-      })
-      .catch(error => {
-        // Capturar y mostrar cualquier error
-        console.error('Error al procesar la solicitud:', error);
-      });
-  });
-  
+document
+    .getElementById("loginForm")
+    .addEventListener("submit", function (event) {
+        event.preventDefault(); // Evitar que el formulario se envíe de forma predeterminada
+
+        // Obtener los valores de usuario y contraseña del formulario
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        // Objeto con los datos del formulario a enviar
+        const formData = {
+            username: username,
+            password: password,
+        };
+
+        // Configurar la solicitud POST
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+        };
+
+        // Realizar la solicitud POST utilizando Fetch API
+        fetch("http://tu-endpoint.com/login", requestOptions)
+            .then((response) => {
+                // Verificar si la respuesta es exitosa
+                if (!response.ok) {
+                    throw new Error(
+                        "Error al iniciar sesión: " + response.statusText
+                    );
+                }
+                // Convertir la respuesta a formato JSON
+                return response.json();
+            })
+            .then((data) => {
+                // Manejar la respuesta JSON
+                console.log("Respuesta del servidor:", data);
+                // Guardar el token en el session storage del navegador
+                sessionStorage.setItem("token", data.token);
+            })
+            .catch((error) => {
+                // Capturar y mostrar cualquier error
+                console.error("Error al procesar la solicitud:", error);
+            });
+    });
--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,26 +7,25 @@ use App\Models\User;
 
 class AuthController extends Controller
 {
-    public function login(Request $request){
+    public function login(Request $request)
+    {
         echo json_encode($_SERVER, JSON_PRETTY_PRINT);
 
         $email = $_SERVER['PHP_AUTH_USER'];
         $clave = $_SERVER['PHP_AUTH_PW'];
 
-
         $user = User::where('email', $email)->first();
 
         if ($user && password_verify($clave, $user->password)) {
             $message = "Inicio de sesión exitoso!";
-            return response()->json(['message' => $message]);
+            $token = $user->remember_token;
+            return response()->json(['message' => $message, 'token' => $token]);
         } elseif (!$user) {
             $message = "El usuario no existe.";
             return response()->json(['message' => $message], 404);
         } else {
             $message = "Usuario o contraseña incorrectos";
             return response()->json(['message' => $message], 401);
-        }   
-
-
+        }
     }
 }
