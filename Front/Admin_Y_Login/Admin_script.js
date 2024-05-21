be40f87887e4b18baa8557b10b126cf4be0504ef
LangeFR <mdjvanegasc@gmail.com>
2024-05-21 14:06:26 -0500

    Fixed cerrar sesion de Admin_script

--- Front/Admin_Y_Login/Admin_script.js
+++ Front/Admin_Y_Login/Admin_script.js
@@ -419,8 +419,9 @@ async function showEntregados() {
 function cerrarSesion() {
     // Ejemplo de cierre de sesión
     sessionStorage.removeItem('token'); // Elimina el token de sesión
+    sessionStorage.removeItem('user_id'); // Elimina el ID de usuario
     alert('Has cerrado sesión correctamente');
   
     // Redirigir a la página de inicio de sesión o a otra página
-    window.location.href = './Roberts.html';
+    window.location.href = 'Roberts.html';
   }
